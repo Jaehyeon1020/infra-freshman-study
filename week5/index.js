0ba0363c6af08d03a3affdec1d0b2012014de0e3
Jaehyeon Kim <65964601+Jaehyeon1020@users.noreply.github.com>
2024-11-05 23:54:46 +0900

    week5 fix

--- week5/index.js
+++ week5/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  console.log("hello world");
+  res.send("hello world");
+});
+
 app.get("/test", (req, res) => {
   console.log("test");
   res.send("test!!");
