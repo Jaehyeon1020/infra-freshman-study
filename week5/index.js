6f903804db6ad9573b51eb91bfa47862e7953748
Jaehyeon Kim <65964601+Jaehyeon1020@users.noreply.github.com>
2024-11-06 00:06:07 +0900

    week5 add

--- week5/index.js
+++ week5/index.js
@@ -11,6 +11,16 @@ app.get("/test", (req, res) => {
   res.send("test!!");
 });
 
+app.get("/api/1", (req, res) => {
+  console.log("api 1");
+  res.send("api 1");
+});
+
+app.get("api/2", (req, res) => {
+  console.log("api 2");
+  res.send("api 2");
+});
+
 app.listen(3000, () => {
   console.log("server running on port 3000");
 });
